1384217ed5723be0f15bd81c178b82cd527a78c7
seuna <seuna@LAPTOP-3O34I0JT>
2020-04-11 13:28:55 +0100

    add steps

---
+++ trying/src/test/java/com/project/Steps/Step.java
@@ -0,0 +1,21 @@
+package com.project.Steps;
+
+import com.project.Helper.BaseClass;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class Step extends BaseClass 
+{
+	@Given("^I navigate to web$")
+	public void i_navigate_to_web() throws Throwable {
+	    
+	}
+
+	@Then("^input email$")
+	public void input_email() throws Throwable {
+	    
+	}
+
+
+}
